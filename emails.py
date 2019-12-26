b5677c15128e70e7806d8891a5d98b65775c772a
Camilo Hernández <chernandez@quimera.dev>
2019-12-26 13:21:36 -0300

    Versión 1.1

--- emails.py
+++ emails.py
@@ -2,10 +2,11 @@
 import os
 import pickle
 import re
+import time
 
 import gspread as gs
 import unidecode as unidecode
-from google.auth.transport import Request
+from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
@@ -13,7 +14,19 @@ from oauth2client.service_account import ServiceAccountCredentials
 ARCHIVO = "Prueba de Emails"
 INICIO_ARCHIVO = 1
 
+TIEMPO_BUFFER = 3  # Segundos
+
 DOMINIO = "@colegio-altamira.cl"
+
+PREFIJO_GENERACIONES = "generacion"
+GENERACIONES = {"5": "2027",
+                "6": "2026",
+                "7": "2025",
+                "8": "2024",
+                "I": "2023",
+                "II": "2022",
+                "III": "2021",
+                "IV": "2020", }
 
 
 def celda_is_null(cell):
@@ -41,7 +54,8 @@ def generate_adresses(names_o):
         for estudiantes in curso:
             x = 3
             while True:
-                email = unidecode.unidecode(estudiantes[0].strip(" ") + "." + estudiantes[1][:x]).lower() + DOMINIO
+                email = unidecode.unidecode(estudiantes[0] + "."
+                                            + estudiantes[1][:x]).lower().replace(" ", "") + DOMINIO
                 if email not in emails_creados:
                     estudiantes.append(email)
                     emails_creados.append(email)
@@ -54,10 +68,19 @@ def generate_adresses(names_o):
     return names
 
 
+def calculate_promotion_groupname(curso):
+    year = GENERACIONES[curso.split("º")[0]]
+    email = PREFIJO_GENERACIONES + year + curso.split("º")[1] + DOMINIO
+    nombre = "%s - %s" % (curso, year)
+
+    return nombre, email
+
+
 print("---------------------------------------")
-print("      Generador de Correos v1.0.0")
+print("      Generador de Correos v1.1.0")
 print("---------------------------------------")
 print()
+print("Mantenido por Camilo Hernández, camilohernandezcueto@gmail.com")
 
 print("INFO: Iniciando..")
 
@@ -65,7 +88,9 @@ print("INFO: Iniciando..")
 SCOPE = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive',
          'https://www.googleapis.com/auth/admin.directory.user']
-SCOPE_GSUITE = ['https://www.googleapis.com/auth/admin.directory.user']
+
+SCOPE_GSUITE = ['https://www.googleapis.com/auth/admin.directory.user',
+                'https://www.googleapis.com/auth/admin.directory.group']
 
 print("INFO: Estableciendo conexión con los servidores..")
 print("INFO: Autenticando..")
@@ -98,7 +123,7 @@ try:
             if len(lista_estudiantes) <= cursor + 1:
                 break
 
-            prox_celda = lista_estudiantes[cursor + 1]
+            prox_celda = lista_estudiantes[cursor + 1].rstrip(" ")
             if celda_is_null(prox_celda):
                 break
 
@@ -147,25 +172,56 @@ print("INFO: Conexión establecida")
 
 default_pass = ask_default_pass()
 
+print("INFO: Generando grupos..")
 print("INFO: Generando correos..")
 
 for curso, estudiantes in generate_adresses(estudiantes_por_curso).items():
+
+    nombre_grupo, email_grupo = calculate_promotion_groupname(curso)
+
+    print("\n\tCreando grupo \"%s\" con el email \"%s\"" % (nombre_grupo, email_grupo))
+    request_grupo_insert = {
+        "email": email_grupo,
+        "name": nombre_grupo,
+        "description": "Estudiantes del curso " + nombre_grupo
+    }
+
+    service.groups().insert(body=request_grupo_insert).execute()
+
     for estudiante in estudiantes:
         print("\n\tCreando cuenta para %s %s, correo: %s" % (estudiante[0], estudiante[1], estudiante[2]))
-        user = {
+
+        email = estudiante[2]
+        request_user = {
             "name": {
                 "familyName": estudiante[1],
                 "givenName": estudiante[0],
             },
             "password": default_pass,
             "changePasswordAtNextLogin": True,
-            "primaryEmail": estudiante[2],
+            "primaryEmail": email,
+            "organizations": [
+                {
+                    "name": "Colegio Altamira",
+                    "title": "Estudiante",
+                    "primary": True,
+                }
+            ],
+        }
+        result_user = service.users().insert(body=request_user).execute()
+
+        print("\tCuenta creada con el ID: %s" % result_user["id"])
+
+        print("\tAñadiendo al grupo")
+        request_grupo_update = {
+            "email": email,
+            "role": "MEMBER"
         }
 
-        result = service.users().insert(body=user).execute()
+        result_grupo_update = service.members().insert(groupKey=email_grupo, body=request_grupo_update).execute()
 
-        print("\tCuenta creada con el ID: %d" % result["id"])
+        time.sleep(TIEMPO_BUFFER)
 
-print("\nINFO: Listo!")
-print("\nINFO: Terminando")
+print("\nINFO: Correos generados con exito")
+print("INFO: Terminando..")
 exit(0)
